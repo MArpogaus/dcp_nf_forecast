c76c2ae054c1d518a144fbb5b5e0eda6b540238d
Marcel Arpogaus <marpogau@htwg-konstanz.de>
2026-05-22 22:29:48 +0000

    chore: remove temp scripts from tracking

--- run_dla.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-export CUDA_VISIBLE_DEVICES=0
-export TF_CPP_MIN_LOG_LEVEL=3
-source /app/.venv/bin/activate
-exec dvc repro -f train@dataset0-normal_baseline train@dataset0-lognormal_baseline train@dataset0-bernstein_nf train@dataset0-bernstein_nf_lognormal train@dataset0-bernstein_nf_scale train@dataset0-bernstein_nf_scale_lognormal train@dataset0-spline_nf train@dataset0-spline_nf_lognormal train@dataset0-spline_nf_scale train@dataset0-spline_nf_scale_lognormal
